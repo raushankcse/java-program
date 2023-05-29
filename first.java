ad5fb57541434b1b8020692f2da608fe93c73a9f
raushan <raushankcse@gmail.com>
2023-05-29 16:03:34 +0530

    star printing program

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first{
+    public static void main(String[] args){
+        for(int i = 10 ;i>0;i--){
+            for(int j= 0; j<i;j++){
+
+                System.out.print("* *");
+            }
+            System.out.println();
+
+        }
+    }
+}
