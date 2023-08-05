eb39299f92184bd387e78b31d2abac4bd25b71bd
raushan <raushankcse@gmail.com>
2023-08-05 08:33:22 +0530

    added some file

---
+++ string/palindrome.java
@@ -0,0 +1,30 @@
+package string;
+
+public class palindrome {
+    public static boolean isPalindrome(String str)
+    {
+        for(int i=0;i<str.length()/2;i++)
+        {
+            int n=str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+
+        }
+
+        return true;
+        
+
+    }
+
+    public static void main(String args[])
+    {
+        
+        String str = "noon";
+        System.out.println(isPalindrome(str));
+    }
+    
+}
+
+
+// time complexity   - O(n)
