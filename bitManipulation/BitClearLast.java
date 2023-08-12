5690e17dba1e0cc5ef24433888d572d0d36519d1
raushan <raushankcse@gmail.com>
2023-08-12 12:18:45 +0530

    bit programming add 2 more more program

---
+++ bitManipulation/BitClearLast.java
@@ -0,0 +1,16 @@
+package bitManipulation;
+
+
+public class BitClearLast {
+
+    public static int clearIBits(int n,int i)
+    {
+        int bitMask = (~0)<<i;
+        return n&bitMask;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(clearIBits(15,2));
+    }
+    
+}
---
+++ bitManipulation/BitClearRange.java
@@ -0,0 +1,16 @@
+package bitManipulation;
+
+public class BitClearRange {
+    public static int clearBitsInRange(int n,int i, int j)
+    {
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitMask = a|b;
+        return n&bitMask;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(clearBitsInRange(10,2,4));
+    }
+    
+}
