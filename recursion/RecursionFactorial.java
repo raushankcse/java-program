6f1e53971ab3cac0b8654c7726270e8ea71bc12f
raushan <raushankcse@gmail.com>
2023-08-17 19:20:06 +0530

    added some file

---
+++ recursion/RecursionFactorial.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class RecursionFactorial {
+    public static int fact(int n){
+        if(n==0)
+        {
+            return 1;
+        }
+
+
+        int fn = n*fact(n-1);
+        return fn;
+    }
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println(fact(n));
+    }
+    
+}
