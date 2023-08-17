b46c1214602802903af5bd8bcf6bdb401e2f140d
raushan <raushankcse@gmail.com>
2023-08-17 19:02:16 +0530

    first commit

--- recursion/Recursion1toN.java
+++ recursion/Recursion1toN.java
@@ -2,5 +2,18 @@ package recursion;
 
 public class Recursion1toN {
 
-    
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+
+    public static void main(String args[])
+    {
+        int n=10;
+        printInc(n);
+    }
 }
