e5471cce3c96c82cfdf2add1ce70cf2e4a5ea03d
raushan <raushankcse@gmail.com>
2023-08-18 09:25:42 +0530

    added some file

---
+++ recursion/RecursionFib.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class RecursionFib{
+    public static int fib(int n)
+    {
+        if(n==0||n==1)
+        {
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1+fnm2;
+        
+        return fn;
+    }
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println(fib(n));
+    }
+}
