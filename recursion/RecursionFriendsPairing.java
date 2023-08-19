d16ea8e3ebbb7d3488adb70c597f2689405f8cdb
raushan <raushankcse@gmail.com>
2023-08-19 10:24:50 +0530

    added some file

---
+++ recursion/RecursionFriendsPairing.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class RecursionFriendsPairing {
+    
+    public static int friendsPairing(int n)
+    {
+        if(n==1 || n==2)
+        {
+            return n;
+        }
+        int fnm1 = friendsPairing(n-1);
+        int fnm2 = friendsPairing(n-2);
+        int pairWays = (n-1) * fnm2;
+        int totWays = fnm1 + pairWays;
+
+        return totWays;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(friendsPairing(2));
+
+    }
+    
+}
