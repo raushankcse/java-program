eca06321cdd02c96de2faf6dccaa5572965c5c3a
raushan <raushankcse@gmail.com>
2023-09-04 19:53:21 +0530

    basics programming

---
+++ dataTypes/Basics1.java
@@ -0,0 +1,8 @@
+public class Basics1 {
+    public static void main(String args[]){
+        System.out.print("Hello world");
+        System.out.println("Hello world");
+        System.out.print("Hello world\n");
+    }
+    
+}
---
+++ dataTypes/Basics4.java
@@ -0,0 +1,14 @@
+public class Basics4 {
+    public static void main(String[] args){
+        byte b = 8;
+        System.out.println(b);
+        char ch='a';
+        System.out.println(ch);
+        boolean var = false;
+        float price = 10.5f;
+        int number =25;
+
+        short n =240;
+    }
+    
+}
