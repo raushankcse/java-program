96e0394bb2fdea1bc6808540e4c49f37377a0521
raushan <raushankcse@gmail.com>
2023-09-28 14:17:12 +0530

    stack

---
+++ stack/Stack3.java
@@ -0,0 +1,15 @@
+package stack;
+import java.util.*;
+public class Stack3 {
+    public static void main(String args[]){
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
